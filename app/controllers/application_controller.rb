c4b325b81a2f67d82c0c242c923e8609fa46d2f1
trinidad cerri <trinidad.cerri@gmail.com>
2023-03-27 12:27:34 +0200

    devise fixed with cloudinary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role, :avatar, :cv, :profile_description])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :role, :avatar, :cv, :profile_description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role, :avatar, :cvs, :profile_description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :role, :avatar, :cvs, :profile_description])
   end
 end
