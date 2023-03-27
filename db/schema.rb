06a23ae0c061f04cce99e360ab02f94a7a6ae890
trinidad cerri <trinidad.cerri@gmail.com>
2023-03-27 19:56:11 +0200

    adding: 10 jobs + update index and show for jobs + add a job description to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_133324) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_171935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -114,6 +114,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_133324) do
     t.string "last_name"
     t.string "role"
     t.string "profile_description"
+    t.string "jobdescription"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
