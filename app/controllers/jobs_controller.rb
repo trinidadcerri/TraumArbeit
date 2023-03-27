4751e4bcdc1eeafb7a6d76ab47df61ab1f8c8bd4
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-03-27 16:38:19 +0300

    Index and show pages for jobs added

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,20 @@
 class JobsController < ApplicationController
+  # before_action :set_job, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [ :index ]
   def index
     @jobs = Job.all
   end
 
+  # def show
+  # end
+
+  # private
+
+  # def set_job
+  #   @job = Job.find(params[:id])
+  # end
+
+  # def job_params
+  #   params.require(:kitchen).permit(:name, :address, :description, :price, :capacity, :photo)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
   root to: "jobs#index"
   resources :jobs do
   resources :appointments, only: [:new, :create, :show]
