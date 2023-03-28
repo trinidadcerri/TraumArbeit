cf38804b901f407e5f9c55083d8ee7db091d4e5c
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-03-28 11:54:50 +0300

    Show page for favorites added

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,36 @@
 class FavoritesController < ApplicationController
+  before_action :set_favorite, only: [:show, :create, :destroy]
+
+  def index
+    @favorites = Favorite.all
+  end
+
+  def show
+  end
+
+  def create
+    @favorite = Favorite.new(review_params)
+    @job = JOb.find(params[:job_id])
+    @favorite.job = @favorite
+    @favorite.user = current_user
+    if @favorite.save
+      redirect_to @job
+    else
+      render 'jobs/show', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @favorite.destroy
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite)
+  end
+
+  def set_favorite
+    @favorite = Favorite.find(params[:id])
+  end
 end
