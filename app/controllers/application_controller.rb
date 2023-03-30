41f53baeb069358041a113879cb94615786c7e7a
trinidad cerri <trinidad.cerri@gmail.com>
2023-03-30 11:46:30 +0200

    after sign in path not working yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role, :avatar, :cvs, :profile_description])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :role, :avatar, :cvs, :profile_description])
   end
+
+  def after_sign_in_path_for(resource)
+  end
 end
