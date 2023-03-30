14f1b37982d29df400a2cb6c2e812a1957d73155
trinidad cerri <trinidad.cerri@gmail.com>
2023-03-30 11:37:21 +0200

    favorites description fixed

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,16 +2,10 @@ class JobsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
   before_action :set_job, only: [:show, :edit, :update, :destroy]
   def index
-    if current_user.job_seeker == false
-      redirect_to candidates_path, notice: "You are not authorized to access this page"
-    end
     @jobs = Job.all
   end
 
   def show
-    if current_user.job_seeker == false
-      redirect_to candidates_path, notice: "You are not authorized to access this page"
-    end
     @favorite = Favorite.new
   end
 
