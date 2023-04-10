fa04b87bce1500409a05bedc462dd4de05f7a5e4
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-04-10 12:21:24 +0300

    Simple calendar added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_075122) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_080222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_075122) do
     t.bigint "job_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.index ["job_id"], name: "index_appointments_on_job_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
