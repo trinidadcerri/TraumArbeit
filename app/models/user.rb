a4964ba2f91582c3ec5a7c7f1f66bf1fde7335f6
trinidad cerri <trinidad.cerri@gmail.com>
2023-04-10 10:36:34 +0200

    user search bar done

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_many_attached :cvs
   has_many :jobs
-  pg_search_scope :search_by_profile_description,
-  against: [ :profile_description ],
+  pg_search_scope :search_by_profesion_and_location,
+  against: [ :profesion, :location ],
   using: {
     tsearch: { prefix: true }
   }
