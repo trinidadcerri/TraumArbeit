780cc7face2601d7cb9dbfb10221155bbc69f03c
trinidad cerri <trinidad.cerri@gmail.com>
2023-04-10 10:30:18 +0200

    search for job positions

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,9 @@
 class Job < ApplicationRecord
+  include PgSearch::Model
   belongs_to :user
+  pg_search_scope :search_by_position_and_address,
+  against: [ :position, :address ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include PgSearch::Model
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
   has_many_attached :cvs
   has_many :jobs
+  pg_search_scope :search_by_profile_description,
+  against: [ :profile_description ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
