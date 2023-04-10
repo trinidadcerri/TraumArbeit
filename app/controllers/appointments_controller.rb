1915595cf606772922360a6a5775a688f14001f0
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-04-10 13:42:22 +0300

    Create new appointment in process

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,34 @@
 class AppointmentsController < ApplicationController
+  before_action :set_appointment, only: [:show, :destroy]
+
+  def index
+    @appointments = Appointment.all
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    @job = Job.find(params[:job_id])
+    @appointment.job = @job
+    @appointment.user = current_user
+    if @appointment.save!
+      redirect_to calendar_path(@job, @appointment)
+  end
+
+  def show
+  end
+
+  def destroy
+    @appointment.destroy
+    redirect_to calendar_path, status: :see_other
+  end
+
+  private
+
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:date, :name, :job_id, :user_id)
+  end
 end
